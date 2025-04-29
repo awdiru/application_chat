71bbbb5a5815dd0d6d9ab7a9a88e04536f81a4f5
avdonin <awdiru@gmail.com>
2025-04-29 13:45:08 +0300

    init commit

---
+++ src/test/java/ru/avdonin/testWebSocket/TestWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.avdonin.testWebSocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
